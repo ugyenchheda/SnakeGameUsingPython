d298e031f2d036e03128b9c24f14581038d28be3
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-27 15:41:58 +0300

    created food and snake

--- main.py
+++ main.py
@@ -8,15 +8,31 @@ SPEED= 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "FF0000"
-BACKGROUND_COLOR = "FFFFFF"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.square = []
+
+        for i in range(0, BODY_PARTS ):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x +SPACE_SIZE, y +SPACE_SIZE, fill = SNAKE_COLOR, tag ="Dangerious Amazonian Snake")
 
 class Food:
-    pass
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)* SPACE_SIZE)
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE) * SPACE_SIZE)
+        
+        self.coordinates = [x, y]
+
+        canvas.create_rectangle(x, y, x + SPACE_SIZE, y +SPACE_SIZE, fill =FOOD_COLOR, tag ="Delicious Food") 
 
 def next_turn():
     pass
@@ -39,5 +55,24 @@ direction = 'down'
 
 label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
 label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height = GAME_HEIGHT, width= GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+#for centering the window in the middle of monitor screen
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x =int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
